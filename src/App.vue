b26e9a7ecc7b7f4511c6eca3e0f7a4199e3af9be
Fernando Dominguez <fdominguez@frd.org.mx>
2022-03-25 16:55:28 -0600

    side menu

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
-  <v-app color="#F3EDBB">
+  <v-app>
     <topbar />
-
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-container fluid>
+      <v-row class="clearfix">
+        <sidemenu class="float-md-start col-auto" />
+        <v-main>
+          <router-view />
+        </v-main>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import topbar from "@/components/basicos/topbar.vue";
+import sidemenu from "@/components/basicos/sidemenu.vue";
 
 export default Vue.extend({
   name: "App",
 
   components: {
     topbar,
+    sidemenu,
   },
 
   data: () => ({
